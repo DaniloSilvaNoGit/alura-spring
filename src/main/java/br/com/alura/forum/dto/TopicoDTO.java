8ab080d1fc29af377d75f526f0ec245827fde017
Danilo Silva de Oliveira <danilo.silva.oliveira0021@gmail.com>
2021-09-02 14:02:31 -0300

    DTO, Models and Repository learn

---
+++ src/main/java/br/com/alura/forum/dto/TopicoDTO.java
@@ -0,0 +1,61 @@
+package br.com.alura.forum.dto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import br.com.alura.forum.modelo.Topico;
+
+public class TopicoDTO {
+
+    private Long id;
+    private String titulo;
+    private String mensagem;
+    private LocalDateTime dataCriacao;
+
+    public TopicoDTO(Topico topico) {
+        this.id = topico.getId();
+        this.titulo = topico.getTitulo();
+        this.mensagem = topico.getMensagem();
+        this.dataCriacao= topico.getDataCriacao();
+    }
+
+    public static List<TopicoDTO> converter(List<Topico> topicoList) {
+        return topicoList.stream().map(TopicoDTO::new).collect(Collectors.toList());
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return this.titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getMensagem() {
+        return this.mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public LocalDateTime getDataCriacao() {
+        return this.dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDateTime dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+
+
+}
